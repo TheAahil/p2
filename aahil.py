83164fba2a38925740958ccad6ed01a151966f88
Rana Aahil <62800395+TheAahil@users.noreply.github.com>
2020-04-09 05:10:47 -0700

    Update aahil.py

--- aahil.py
+++ aahil.py
@@ -177,8 +177,8 @@ def menu():
 	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m Nama \033[1;91m: \033[1;92m"+nama+"\033[1;97m               "
 	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m ID   \033[1;91m: \033[1;92m"+id+"\x1b[1;97m              "
 	print 42*"\033[1;96m="
-	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;36m Hack USA"            "
-	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Logout            "
+	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;36m Hack USA"
+	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Logout"
 	pilih()
 
 
